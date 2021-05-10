ab855570172c779c03db11d182e1583bf5acca4a
Xabi <xabi.aycaguer@gmail.com>
2021-05-10 19:15:05 +0200

    v1

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,7 +1,24 @@
 class StaticsController < ApplicationController
+
+  def home
+    @gossips = Gossip.all
+  end
+
   def team
   end
 
   def contact
   end
+
+  def welcome
+  end
+
+  def show
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def user_show
+    @user = User.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-
-  get '/team',    to: 'statics#team'
-  get '/contact', to: 'statics#contact'
+  root 'statics#home'
+  get '/team',                to: 'statics#team'
+  get '/contact',             to: 'statics#contact'
+  get '/welcome/:first_name', to: 'statics#welcome'
+  get '/gossips/:id',         to: 'statics#show', as:'gossip'
+  get '/users/:id',           to: 'statics#user_show', as:'user'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
